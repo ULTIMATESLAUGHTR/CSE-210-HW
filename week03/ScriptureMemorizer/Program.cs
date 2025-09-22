27a3651cc2e7d62b51961a156b02c196b79c2054
ULTIMATESLAUGHTER <aislas1@byupathway.edu>
2025-09-22 10:33:30 -0600

    corrected error on ScriptureHider which only removed 1-3 words but didn't yet hide the entire scripture.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -16,5 +16,31 @@ class Program
         {
             return;
         }
+        else
+        {
+
+            while (true)
+            {
+                Console.Clear();
+                var hider = new ScriptureHider(randomScripture.Text);
+                hider.HideRandomWords(2);
+                Console.WriteLine($"{randomScripture.Reference}: {hider.GetHiddenText()}");
+
+                if (hider.GetHiddenText().Contains("_"))
+                {
+                    Console.WriteLine("\nPress Enter to hide more words or type 'quit' to quit.");
+                    if (Console.ReadLine().ToLower() == "quit")
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nAll words are hidden! Press Enter one more time to exit the program and get a new scripture to memorize.");
+                    Console.ReadLine();
+                    break;
+                }
+        }
+        }
     }
 }
--- week03/ScriptureMemorizer/ScriptureHider.cs
+++ week03/ScriptureMemorizer/ScriptureHider.cs
@@ -1,39 +1,62 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class ScriptureHider
 {
     private string _text;
-    private string _hiddenText;
+    private List<string> _words;
+    private HashSet<int> _hiddenWordIndices;
     private Random _random;
 
-    private ScriptureHider(string text)
+    public ScriptureHider(string text)
     {
         _text = text;
-        _hiddenText = text;
+        _words = text.Split(' ').ToList();
+        _hiddenWordIndices = new HashSet<int>();
         _random = new Random();
     }
-    private string GetHiddenText()
+    public string GetHiddenText()
     {
-        return _hiddenText;
-    }
-    private void HideRandomWords(int count)
-    {
-        count = Math.Max(1, Math.Min(3, count));
-        var words = _hiddenText.Split(' ');
-        int hiddenCount = 0;
-        int maxAttempts = words.Length * 2; // Prevent infinite loop
-        int attempts = 0;
-
-        while (hiddenCount < count && attempts < maxAttempts)
+        var displayWords = new List<string>();
+        for (int i = 0; i < _words.Count; i++)
         {
-            int index = _random.Next(words.Length);
-            if (words[index] != "____")
+            if (_hiddenWordIndices.Contains(i))
             {
-                words[index] = "____";
-                hiddenCount++;
+                displayWords.Add(new string('_', _words[i].Length));
+            }
+            else
+            {
+                displayWords.Add(_words[i]);
             }
         }
+        return string.Join(" ", displayWords);
+    }
+    public void HideRandomWords(int count)
+    {
+        count = Math.Max(1, Math.Min(3, count));
+        var availableIndices = new List<int>();
+        for (int i = 0; i < _words.Count; i++)
+        {
+            if (!_hiddenWordIndices.Contains(i))
+            {
+                availableIndices.Add(i);
+            }
+        }
+        int wordsToHide = Math.Min(count, availableIndices.Count);
+        for (int i = 0; i < wordsToHide; i++)
+        {
+            int randomIndex = _random.Next(availableIndices.Count);
+            int wordIndex = availableIndices[randomIndex];
+            _hiddenWordIndices.Add(wordIndex);
+            availableIndices.RemoveAt(randomIndex);
+        }
+    }
 
-        _hiddenText = string.Join(" ", words);
+    public bool AllWordsHidden => _hiddenWordIndices.Count == _words.Count;
+
+    public string GetOriginalText()
+    {
+        return _text;
     }
 }
