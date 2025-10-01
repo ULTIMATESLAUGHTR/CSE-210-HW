cb56337ffaf33cf5f7bca6b6c2d121d6e5e57d72
ULTIMATESLAUGHTER <aislas1@byupathway.edu>
2025-10-01 09:29:52 -0600

    HomeworkAssignment classes created, tested and passed.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignments a1 = new Assignments("John Doe", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Jane Smith", "Fractions", "7.3", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+
+        WritingAssignment a3 = new WritingAssignment("Emily Johnson", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
